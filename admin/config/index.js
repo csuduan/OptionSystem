cb127db284f38a55b006702d7fed8024bbf06b5b
csuduan <csuduan@qq.com>
2018-03-13 18:32:28 +0800

    修改代理地址

--- admin/config/index.js
+++ admin/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api/*':{
-          target: 'http://192.168.1.38:5000',
+          target: 'http://127.0.0.1:5000',
           pathRewrite: {"^/api" : ""}
       }
     },
